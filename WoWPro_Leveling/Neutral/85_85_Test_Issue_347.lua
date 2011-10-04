30bb870bca7b98703bdff54dc13683628e697cb0
LaoTseu <eric.beaudoin@gmail.com>
2011-10-04 18:12:13 -0400

    New test guide for issue https://github.com/Jiyambi/WoW-Pro-Guides/issues/347

---
+++ WoWPro_Leveling/Neutral/85_85_Test_Issue_347.lua
@@ -0,0 +1,77 @@
+WoWPro.Leveling:RegisterGuide('LaoTseuTest3478585', 'Molten Front', 'LaoTseu', '85', '85', nil, 'Neutral', function()
+return [[
+
+N Test Guide - Issue 347 |N|This is a mini guide to help test the Issue #347 with Quest Objectives not completing when the step is sticky.|
+
+A Call the Flock|QID|29147|S|
+A Rage Against the Flames|QID|29123|
+A Call the Flock|QID|29147|US|
+
+C Call the Flock|QID|29147|QO|Alpine Songbird gathered: 12/12|S|
+C Call the Flock|QID|29147|QO|Forest Owl: 5/5|S|
+C Call the Flock|QID|29147|QO|Goldwing Hawk: 2/2|S|
+
+C Rage Against the Flames|QID|29123|
+
+C Call the Flock|QID|29147|QO|Goldwing Hawk: 2/2|US|
+C Call the Flock|QID|29147|QO|Forest Owl: 5/5|US|
+C Call the Flock|QID|29147|QO|Alpine Songbird gathered: 12/12|US|
+
+T Call the Flock|QID|29147|S|
+T Rage Against the Flames|QID|29123|
+T Call the Flock|QID|29147|US|
+
+A The Flame Spider Queen|QID|29276|S|
+A Pyrorachnophobia|QID|29159|
+A The Flame Spider Queen|QID|29276|US|
+
+C Flame Venom|QID|29276|QO|Flame Venom: 8/8|S|
+C Searing Web Fluid|QID|29276|QO|Searing Web Fluid: 8/8|S|
+C The Flame Spider Queen|QID|29276|L|69808 8|S|
+C Searing Web Fluid|QID|29276|L|69809 8|S|
+
+C Pyrorachnophobia|QID|29159|
+
+C Searing Web Fluid|QID|29276|L|69809 8|US|
+C The Flame Spider Queen|QID|29276|L|69808 8|US|
+C Searing Web Fluid|QID|29276|QO|Searing Web Fluid: 8/8|US|
+C Flame Venom|QID|29276|QO|Flame Venom: 8/8|US|
+
+
+T The Flame Spider Queen|QID|29276|S|
+T Pyrorachnophobia|QID|29159|
+T The Flame Spider Queen|QID|29276|US|
+
+A Fandral's Methods|QID|29275|S|
+A Enduring the Heat|QID|29210|
+A Fandral's Methods|QID|29275|US|
+
+C Fandral's Methods|QID|29275|QO|Flame Druid Staff: 1/1|S|
+C Fandral's Methods|QID|29275|QO|Flame Druid Spellbook: 1/1|S|
+C Fandral's Methods|QID|29275|QO|Flame Druid Reagent Pouch: 1/1|S|
+C Fandral's Methods|QID|29275|QO|Flame Druid Idol: 1/1|S|
+
+C Fandral's Methods|QID|29275|L|69810|N|Flame Druid Staff|S|
+C Fandral's Methods|QID|29275|L|69811|N|Flame Druid Spellbook|S|
+C Fandral's Methods|QID|29275|L|69812|N|Flame Druid Reagent Pouch|S|
+C Fandral's Methods|QID|29275|L|69813|N|Flame Druid Idol|S|
+
+C Enduring the Heat|QID|29210|
+
+C Fandral's Methods|QID|29275|QO|Flame Druid Idol: 1/1|US|
+C Fandral's Methods|QID|29275|QO|Flame Druid Reagent Pouch: 1/1|US|
+C Fandral's Methods|QID|29275|QO|Flame Druid Spellbook: 1/1|US|
+C Fandral's Methods|QID|29275|QO|Flame Druid Staff: 1/1|US|
+
+C Fandral's Methods|QID|29275|L|69810|N|Flame Druid Staff|US|
+C Fandral's Methods|QID|29275|L|69811|N|Flame Druid Spellbook|US|
+C Fandral's Methods|QID|29275|L|69812|N|Flame Druid Reagent Pouch|US|
+C Fandral's Methods|QID|29275|L|69813|N|Flame Druid Idol|US|
+
+T Fandral's Methods|QID|29275|S|
+T Enduring the Heat|QID|29210|
+T Fandral's Methods|QID|29275|US|
+
+]]
+
+end)
