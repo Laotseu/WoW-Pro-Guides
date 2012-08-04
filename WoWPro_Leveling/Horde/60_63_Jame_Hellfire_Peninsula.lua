80166cb32e54321b962b29beb66aeb534bdad97b
LaoTseu <eric.beaudoin@gmail.com>
2012-08-04 01:03:05 -0400

    Play testing the Horde Hellfire Peninsula guide

--- WoWPro_Leveling/Horde/60_63_Jame_Hellfire_Peninsula.lua
+++ WoWPro_Leveling/Horde/60_63_Jame_Hellfire_Peninsula.lua
@@ -47,8 +47,7 @@ A I Work... For the Horde! |QID|10086|M|55.2,38.8|N|From Megzeg Nukklebust.|
 N Flying|QID|10809|N|Expert Riding may be purchased for 225g, you will also need 50g for a mount. Wind Rider Jahubo.|M|54.2,41.6|
 T Eradicate the Burning Legion |QID|10121|N|To Sergeant Shatterskull.|M|58.0,41.5|
 A Felspark Ravine |QID|10123|M|58.0,41.5|N|From Sergeant Shatterskull.|
-; The quest "Invading the Citadel" on the following line has wrong caps on 'The' so it does not auto-accept since it is optional.|
-A Invading The Citadel|QID|29542|M|61.7,60.8|N|From Sergeant Shatterskull. This quest leads into Hellfire Ramparts, this is completely optional, there will be no follow up for it. Once you take it, feel free to join queue for Hellfire Ramparts if you wish.|
+A Invading the Citadel|QID|29542|M|58.0,41.5|N|From Sergeant Shatterskull. This quest leads into Hellfire Ramparts, this is completely optional, there will be no follow up for it. Once you take it, feel free to join queue for Hellfire Ramparts if you wish.|
 C Felspark Ravine |QID|10123|N|Kill the needed demons.|M|65,36|
 C Bonechewer Blood |QID|10450|M|56.5,47.0|N|Kill and loot orcs for their blood.|S|
 C I Work... For the Horde! |QID|10086|N|Pick up the sparkling pieces of wood and metal.|M|60,45|
@@ -107,7 +106,7 @@ A Forge Camp: Mageddon |QID|10390|M|55.0,36.0|N|From Nazgrel.|
 A The Assassin |QID|9400|M|55.0,36.0|N|From Nazgrel.|
 T Apothecary Antonivich |QID|10835|N|To Apothecary Antonivich.|M|52,36|
 
-A Burden of Souls |QID|10864|M|52.5,36.5|N|From Apothecary Antonivich.|
+A A Burden of Souls |QID|10864|M|52.5,36.5|N|From Apothecary Antonivich.|
 C Forge Camp: Mageddon |QID|10390|N|Kill Gan'arg Servants. Also kill and loot Razorsaw.|M|64,31|
 A Vile Plans |QID|10393|U|29590|M|64.5,32.0|N|From the Burning Legion Missive you picked up.|
 T Forge Camp: Mageddon |QID|10390|M|55.0,36.0|N|To Nazgrel.|
@@ -151,13 +150,14 @@ A Ravager Egg Roundup |QID|9349|M|49.0,75.0|N|From Legassi.|
 C Ravager Egg Roundup |QID|9349|N|Get 12 Ravager eggs from the ground and the Ravagers.|M|42,83|
 T Ravager Egg Roundup |QID|9349|M|49.0,75.0|N|To Legassi.|
 A Helboar, the Other White Meat |QID|9361|M|49.0,75.0|N|From Legassi.|
+C In Case of Emergency... |QID|10161|N|Pick up Zepplin Debris.|M|58.0,71.5|S|
+K Crust Bursters|QID|9373|S|L|23338|M|49,67|N|Kill the Crust Bursters on this area until you loot an Eroded Leather Case.|
 C Helboar, the Other White Meat |QID|9361|N|Head to the NE and work your way south, you should find plenty of Deranged Helboars, kill and loot them. Use the Purification Mixture on the Tainted Helboar Meat.|U|23268|M|50.0,75.0|
-K Crust Bursters|QID|9373|L|23338|N|Kill the Crust Bursters on this area until you loot an Eroded Leather Case.|
+K Crust Bursters|QID|9373|US|L|23338|M|49,67|N|Kill the Crust Bursters on this area until you loot an Eroded Leather Case.|
 A Missing Missive |QID|9373|U|23338|M|48.0,66.0|N|From Eroded Leather Case.|
-T Helboar, the Other White Meat |QID|9361|M|49.0,75.0|N|To Legassi.|
+T Helboar, the Other White Meat |QID|9361|M|49.2,74.8|N|To Legassi.|
 
 A Smooth as Butter |QID|9356|M|49.0,75.0|N|From Legassi.|
-C In Case of Emergency... |QID|10161|N|Pick up Zepplin Debris.|M|58.0,71.5|S|
 C Make Them Listen |QID|10220|N|Kill Unyielding Footman, Knights and Sorcerers.|S|
 l A Mysterious Tome |QID|10229|N|Kill Unyielding Knights at the Expedition Armory until they drop the item "A Mysterious Tome".|L|28552|M|55,76|
 C Make Them Listen |QID|10220|N|Kill Unyielding Footman, Knights and Sorcerers.|US|
@@ -184,7 +184,7 @@ T Decipher the Tome |QID|10229|M|61.74,81.79|N|To Althen the Historian.|
 A The Battle Horn |QID|10230|M|61.74,81.79|N|From Althen the Historian.|
 T Make Them Listen |QID|10220|N|To Emissary Mordiba.|M|61.78,81.71|
 C The Battle Horn|QID|10230|N|Head south to the Expedition Armory. Kill and loot Lieutenant Commander Thalvos.|M|54.8,83.7|
-T Smooth as Butter |QID|9356|N|To Legassi.|M|49,75|
+T Smooth as Butter |QID|9356|N|To Legassi.|M|49.2,74.8|
 T In Case of Emergency... |QID|10161|M|49.0,75.0|N|To "Sreaming" Screed Luckheed.|
 A Voidwalkers Gone Wild |QID|9351|M|49.0,75.0|N|From "Sreaming" Screed Luckheed.|
 C Voidwalkers Gone Wild |QID|9351|N|Kill and loot Voidwalkers until you have 10 Essences.|M|49.5,82.0|S|
