ad1b9af6060502ef1bac02c6e18e2785e880357f
Laotseu <eric.beaudoin@gmail.com>
2012-08-20 20:28:05 -0400

    New Alliance The Jade Forest guide.

---
+++ WoWPro_Leveling/Alliance/85_86_Laotseu_The_Jade_Forest.lua
@@ -0,0 +1,195 @@
+﻿
+-- WoWPro Guides by "The WoW-Pro Community" are licensed under a Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License.
+-- Based on a work at github.com.
+-- Permissions beyond the scope of this license may be available at http://www.wow-pro.com/License.
+
+
+WoWPro.Leveling:RegisterGuide('LaotseuTJF8586', 'The Jade Forest', 'Laotseu', '85', '86', nil, 'Alliance', function()
+return [[
+
+A The King's Command |QID|29547|N|The quest should pup-up automatically when you are in Stormwind|
+C The King's Command |QID|29547|M|79.4,39.3|Z|Stormwind City|; 
+T The King's Command |QID|29547|N|To Rell Nightwind|M|78.9,39.8|Z|Stormwind City|; 
+A The Mission |QID|29548|N|From Rell Nightwind|M|78.9,39.8|Z|Stormwind City|; 
+C The Mission |QID|29548|NC|M|66.0,71.0|Z|Stormwind City|; Northern Elwynn Mountains
+
+T The Mission |QID|29548|N|To Sky Admiral Rogers|M|42.0,92.8|; The Skyfire
+A Unleash Hell |QID|31732|N|From Sky Admiral Rogers|M|42.0,92.8|; The Skyfire
+C Unleash Hell |QID|31732|N|Board a copter, kill horde and destroy some boat. You just have to shoot, the copter drives itself.|
+T Unleash Hell |QID|31732|N|To Sky Admiral Rogers|M|42.0,92.8|; The Skyfire
+
+A Touching Ground |QID|31733|N|From Sky Admiral Rogers|M|42.0,92.8|; The Skyfire
+C Touching Ground |QID|31733|NC|M|42.3,92.7|N|Put on the parachute and jump.|
+
+T Touching Ground |QID|31733|N|To Sully "The Pickle" McLeary|M|43.6,90.6|; Garrosh'ar Point
+A No Plan Survives Contact with the Enemy |QID|30069|N|From Sully "The Pickle" McLeary|M|43.6,90.6|; Garrosh'ar Point
+A Welcome Wagons |QID|31734|N|From Sully "The Pickle" McLeary|M|43.6,90.6|; Garrosh'ar Point - Need better coord
+
+C No Plan Survives Contact with the Enemy |QID|30069|S|M|44.4,92.9|; Garrosh'ar Point - Need better coord
+C Welcome Wagons |QID|31734|U|89612|M|44.0,94.3|; Garrosh'ar Point - Need better coord
+C No Plan Survives Contact with the Enemy |QID|30069|US|M|44.4,92.9|; Garrosh'ar Point
+
+T No Plan Survives Contact with the Enemy |QID|30069|N|To Rell Nightwind|M|45.1,95.0|; Garrosh'ar Point
+T Welcome Wagons |QID|31734|N|To Rell Nightwind|M|45.1,95.0|; Garrosh'ar Point
+A The Right Tool For The Job |QID|31735|N|From Rell Nightwind|M|45.1,95.0|; Garrosh'ar Point
+
+C Destroy the first barricade |QID|31735|U|89624|QO|First Barricade Destroyed: 1/1|M|45.6,95.2|N|Use the flaregun to ignite the powder and destroy the first baricade. In the beta, this quest was sometime buged and required to relog in order to have the barricades corretly phased with you.|; Garrosh'ar Point
+C Destroy the second barricade |QID|31735|U|89624|QO|Second Barricade Destroyed: 1/1|M|45.9,95.9|N|Use the flaregun to ignite the powder and destroy the second baricade|; Garrosh'ar Point
+K Confront Ga'trul |QID|31735|QO|Ga'trul Confronted: 1/1|M|46.3,96.3|N|Attack Ga'trul until he teleport away|; Garrosh'ar Point
+
+T The Right Tool For The Job |QID|31735|N|To Rell Nightwind|M|46.5,96.3|; Garrosh'ar Point
+A Envoy of the Alliance |QID|31736|N|From Rell Nightwind|M|46.5,96.3|; Garrosh'ar Point
+A The Cost of War |QID|31737|N|From Rell Nightwind|M|46.5,96.3|; Garrosh'ar Point
+
+C The Cost of War |QID|31737|M|44.3,90.8|N|Kill Sha Hunts and Sha Harbingers|; Garrosh'ar Point - Need better coord
+C Envoy of the Alliance |QID|31736|NC|M|45.9,85.0|; Paw'don Village
+
+T The Cost of War |QID|31737|N|To Taran Zhu|M|46.2,84.8|; Paw'don Village
+T Envoy of the Alliance |QID|31736|N|To Sunke Khang|M|46.2,84.8|; Paw'don Village
+A Pillaging Peons |QID|31738|N|From Sunke Khang|M|46.2,84.8|; Paw'don Village
+A Priorities! |QID|31739|N|From Teng Applebloom|M|46.2,84.7|; Paw'don Village
+A Critical Condition |QID|29552|N|From Mishka|M|46.1,84.6|; Paw'don Village
+h Paw'don Village |N|Talk to Jiayi Applebloom|M|44.9,84.4|N|Paw'don is a good place to set your heart since profession trainners are present and there is a portal to StormWind in the village|; Paw'don Village
+
+C Pillaging Peons |QID|31738|S|M|42.6,86.8|; Paw'don Glade - Need better coord
+C Critical Condition |QID|29552|S|NC|U|82829|M|42.1,89.6|N|Any bandage or healing spell will work. You need to provide your own bandages.|; Paw'don Glade - Need better coord
+C Priorities! |QID|31739|NC|M|42.3,88.7|N|Click on the applebloom cider barels and let Teng Applebloom pick them up. It's quite impressive to see him carry them all!|; Paw'don Glade - Need better coord
+
+T Priorities! |QID|31739|N|To Lin Applebloom|M|39.5,90.0|; Paw'don Glade
+A Koukou's Rampage |QID|31740|N|From Lin Applebloom|M|39.5,90.0|; Paw'don Glade
+
+K Koukou |QID|31740|T|Koukou|M|39.4,89.9|N|Put the poor Koukou out of his misery|; Paw'don Glade
+
+T Koukou's Rampage |QID|31740|N|To Sunke Khang|M|46.2,84.7|; Paw'don Village
+A Twinspire Keep |QID|31741|N|From Sunke Khang|M|46.2,84.7|; Paw'don Village
+
+C Critical Condition |QID|29552|S|NC|US|82829|M|42.1,89.6|N|Any bandage or healing spell will work. You need to provide your own bandages.|; Paw'don Glade - Need better coord
+C Pillaging Peons |QID|31738|US|M|42.6,86.8|; Paw'don Glade - Need better coord
+
+T Critical Condition |QID|29552|N|To Mishka|M|46.1,84.5|; Paw'don Village
+T Pillaging Peons |QID|31738|N|To Sunke Khang|M|46.2,84.7|; Paw'don Village
+A Unfair Trade |QID|31744|N|From Sunke Khang|M|46.2,84.7|; Paw'don Village
+A Fractured Forces |QID|31742|N|From Rell Nightwind|M|46.2,84.8|; Paw'don Village
+A Smoke Before Fire |QID|31743|N|From Rell Nightwind|M|46.2,84.8|; Paw'don Village
+
+C Twinspire Keep |QID|31741|S|M|41.6,81.3|; Twinspire Keep
+C Unfair Trade |QID|31744|S|M|40.3,82.0|N|Kill the gree demonic eyeballs to free the cubs|; Twinspire Keep - Need better coord
+K Dalgan slain |QID|31742|S|T|Dalgan|QO|Dalgan slain: 1/1|M|41.0,80.2|; Twinspire Keep
+K Bellandra Felstorm |QID|31742|S|T|Bellandra Felstorm|QO|Bellandra Felstorm slain: 1/1|M|41.7,80.2|; Twinspire Keep
+C Bomb the Twinspire munitions stockpile |QID|31743|NC|U|89602|QO|Twinspire munitions stockpile bombed: 1/1|M|40.6,82.4|N|Use the flare to call an arial strike|; Twinspire Keep
+C Bomb Xhu'daggab |QID|31743|NC|U|89602|QO|Xhu'daggab bombed: 1/1|M|40.3,82.0|N|Use the flare to call an arial strike|; Twinspire Keep
+C Bomb the Wild Imps |QID|31743|NC|U|89602|QO|Wild Imps bombed: 1/1|M|43.3,81.5|N|Use the flare to call an arial strike|; Twinspire Keep
+C Bomb the Twinspire Demolishers |QID|31743|NC|U|89602|QO|Twinspire Demolishers bombed: 1/1|M|41.2,81.4|N|Use the flare to call an arial strike|; Twinspire Keep
+K Bellandra Felstorm |QID|31742|US|T|Bellandra Felstorm|QO|Bellandra Felstorm slain: 1/1|M|41.7,80.2|; Twinspire Keep
+K Dalgan slain |QID|31742|US|T|Dalgan|QO|Dalgan slain: 1/1|M|41.0,80.2|; Twinspire Keep
+C Unfair Trade |QID|31744|US|M|40.3,82.0|N|Kill the gree demonic eyeballs to free the cubs|; Twinspire Keep - Need better coord
+C Twinspire Keep |QID|31741|US|M|41.6,81.3|; Twinspire Keep
+T Twinspire Keep |QID|31741|N|To Sully "The Pickle" McLeary|M|41.4,79.6|; Twinspire Keep
+T Unfair Trade |QID|31744|N|To Sully "The Pickle" McLeary|M|41.4,79.6|; Twinspire Keep
+T Fractured Forces |QID|31742|N|To Sully "The Pickle" McLeary|M|41.4,79.6|; Twinspire Keep
+T Smoke Before Fire |QID|31743|N|To Sully "The Pickle" McLeary|M|41.4,79.6|; Twinspire Keep
+A The Fall of Ga'trul |QID|30070|N|From Sully "The Pickle" McLeary|M|41.4,79.6|; Twinspire Keep
+K Ga'trul |QID|30070|T|Ga'trul|M|41.4,78.6|Z|Twinspire Keep|; 
+T The Fall of Ga'trul |QID|30070|N|To Rell Nightwind|M|41.5,79.1|Z|Twinspire Keep|; 
+A Onward and Inward |QID|31745|N|From Rell Nightwind|M|41.5,79.1|Z|Twinspire Keep|; 
+C Onward and Inward |QID|31745|NC|M|41.5,79.6|N|Use the gyrocopter to get a ride to the village|; Paw'don Village
+
+T Onward and Inward |QID|31745|N|To Sky Admiral Rogers|M|48.0,88.4|; Paw'don Village
+A The White Pawn |QID|29555|N|From Sky Admiral Rogers|M|48.0,88.4|; Paw'don Village
+A Hozen Aren't Your Friends, Hozen Are Your Enemies |QID|29556|N|From Sky Admiral Rogers|M|48.0,88.4|; Paw'don Village
+
+C Hozen Aren't Your Friends, Hozen Are Your Enemies |QID|29556|S|M|50.5,89.6|; Wreck of the Vanguard
+C The White Pawn |QID|29555|T|Hozen Diver|M|50.0,90.3|N|The first clue is the guard on the bridge. The second and third clues are the banner and the journal underwater in the ship. You should also see a Hozen Diver inside the ship, you need to kill it to get the Encoded Captain's Log.|; Wreck of the Vanguard
+C Hozen Aren't Your Friends, Hozen Are Your Enemies |QID|29556|US|M|50.5,89.6|; Wreck of the Vanguard
+
+T The White Pawn |QID|29555|N|To Nodd Codejack|M|50.4,88.2|; 
+T Hozen Aren't Your Friends, Hozen Are Your Enemies |QID|29556|N|To Nodd Codejack|M|50.4,88.2|; 
+A The Missing Admiral |QID|29553|N|From Nodd Codejack|M|50.4,88.2|; 
+
+T The Missing Admiral |QID|29553|N|To Admiral Taylor|M|54.2,82.4|; Slingtail Pits
+A The Path of War |QID|29558|N|From Bold Karasshi|M|54.2,82.4|; Slingtail Pits
+A Freeing Our Brothers |QID|29559|N|From Bold Karasshi|M|54.2,82.4|; Slingtail Pits
+A Ancient Power |QID|29560|N|From Bold Karasshi|M|54.2,82.4|; Slingtail Pits
+
+C The Path of War |QID|29558|S|M|53.7,80.3|; Slingtail Pits
+C Ancient Power |QID|29560|S|NC|M|53.8,81.1|N|The skulls are lying on the ground in the village|; Slingtail Pits
+C Freeing Our Brothers |QID|29559|M|54.6,79.3|N|You need to kill Slingtail to get the keys that open the cages|; Slingtail Pits
+C Ancient Power |QID|29560|US|NC|M|53.8,81.1|N|The skulls are lying on the ground in the village|; Slingtail Pits
+C The Path of War |QID|29558|US|M|53.7,80.3|; Slingtail Pits
+
+T The Path of War |QID|29558|N|To Bold Karasshi|M|54.2,82.4|; Slingtail Pits
+T Freeing Our Brothers |QID|29559|N|To Bold Karasshi|M|54.2,82.4|; Slingtail Pits
+T Ancient Power |QID|29560|N|To Bold Karasshi|M|54.2,82.4|; Slingtail Pits
+A Kung Din |QID|29759|N|From Bold Karasshi|M|54.2,82.4|; Slingtail Pits
+
+K Kung Din |QID|29759|M|54.6,80.1|T|Kung Din|N|Kill Kung Din to get the Singtail Key|; Slingtail Pits
+
+T Kung Din |QID|29759|N|To Bold Karasshi|M|54.1,82.4|; Slingtail Pits
+A Jailbreak |QID|29562|N|From Bold Karasshi|M|54.1,82.4|; Slingtail Pits
+
+C Jailbreak |QID|29562|M|59.0,81.7|N|On your way to the Pearlfin Village, kill Singtail Fishripper.|; Slingtail Pits
+
+T Jailbreak |QID|29562|N|To Bold Karasshi|M|59.0,81.7|; Pearlfin Village
+A The Pearlfin Situation |QID|29883|N|From Rell Nightwind|M|58.9,81.9|; Pearlfin Village
+A Road Rations |QID|29885|N|From Rell Nightwind|M|58.9,81.9|; Pearlfin Village
+
+C Speak to Pearlkeeper Fujin |QID|29883|NC|QO|Speak to Pearlkeeper Fujin: 1/1|M|58.8,81.3|; Pearlfin Village
+C Speak to Elder Lusshan |QID|29883|NC|QO|Speak to Elder Lusshan: 1/1|M|58.0,80.6|; Pearlfin Village
+C Speak to Instructor Sharpfin |QID|29883|NC|QO|Speak to Instructor Sharpfin: 1/1|M|58.8,84.6|; Pearlfin Village
+C Speak to Watersmith Ot-Temmdo |QID|29883|NC|QO|Speak to Watersmith Ot-Temmdo: 1/1|M|59.9,83.9|; Pearlfin Village
+C Road Rations |QID|29885|M|60.6,85.0|N|Kill the Pearly Tortoises on the outher bank of the lake around the Pearlfin Village|; Pearlfin Village
+
+T The Pearlfin Situation |QID|29883|N|To Rell Nightwind|M|58.9,81.9|; Pearlfin Village
+T Road Rations |QID|29885|N|To Rell Nightwind|M|58.9,81.9|; Pearlfin Village
+A Family Heirlooms |QID|29762|N|From Bold Karasshi|M|59.0,81.7|; Pearlfin Village
+A The Elder's Instruments |QID|29887|N|From Pearlkeeper Fujin|M|58.8,81.3|; Pearlfin Village
+
+C The Elder's Instruments |QID|29887|S|M|66.3,86.6|N|Kill the monkeys until you get all four items|; Moonwater Retreat
+C Family Heirlooms |QID|29762|NC|M|66.7,86.7|N|Loot the corpses around the village|; Moonwater Retreat
+C The Elder's Instruments |QID|29887|US|M|66.3,86.6|N|Kill the monkeys until you get all four items|; Moonwater Retreat
+
+T The Elder's Instruments |QID|29887|N|To Pearlkeeper Fujin|M|58.8,81.3|; Pearlfin Village
+T Family Heirlooms |QID|29762|N|To Bold Karasshi|M|59.0,81.7|; Pearlfin Village
+A Spirits of the Water |QID|29894|N|From Pearlkeeper Fujin|M|58.8,81.3|; Pearlfin Village
+
+C Spirits of the Water |QID|29894|QO|Speak to Elder Lusshan: 1/1|M|58.6,82.9|; Pearlfin Village
+C Spirits of the Water |QID|29894|QO|Witness the waterspeaking ceremony: 1/1|M|58.6,82.9|; Pearlfin Village
+C Spirits of the Water |QID|29894|M|58.6,82.9|; Pearlfin Village
+
+T Spirits of the Water |QID|29894|N|To Bold Karasshi|M|59.0,81.7|; Pearlfin Village
+A SI:7 Report: Lost in the Woods |QID|29733|N|From Rell Nightwind|M|49.6,72.6|; Pearlfin Village
+C SI:7 Report: Lost in the Woods |QID|29733|M|49.8,70.8|N|Use all three of Rell Nightwind's attack to kill the elite Hozen and then General Rik-Rik. Hint, if you are about to die, exit the vehicle and it will take you back to the start without dieing.|; Fox Grove
+T SI:7 Report: Lost in the Woods |QID|29733|N|To Rell Nightwind|M|59.0,81.8|; Pearlfin Village
+A SI:7 Report: Fire From the Sky |QID|29725|N|From Sully "The Pickle" McLeary|M|51.0,63.1|; Shadybranch Pocket
+C Scout the southeastern shrine |QID|29725|QO|Scout the southeastern shrine: 1/1|M|50.7,62.7|N|Whenever you spot a tiger, use the Light Scope to have Amber Kearnen kill it for you.|; Serpent's Heart
+C Scout the southwestern shrine |QID|29725|QO|Scout the southwestern shrine: 1/1|M|46.4,62.0|N|Whenever you spot a tiger, use the Light Scope to have Amber Kearnen kill it for you.|; Serpent's Heart
+C Scout the northern shrine |QID|29725|QO|Scout the northern shrine: 1/1|M|47.4,58.6|N|Whenever you spot a tiger, use the Light Scope to have Amber Kearnen kill it for you.|; Serpent's Heart
+C Return to camp |QID|29725|QO|Return to camp: 1/1|M|50.9,63.0|N|Whenever you spot a tiger, use the Light Scope to have Amber Kearnen kill it for you.|; Shadybranch Pocket
+T SI:7 Report: Fire From the Sky |QID|29725|N|To Sully "The Pickle" McLeary|M|58.9,81.8|; Pearlfin Village
+A SI:7 Report: Hostile Natives |QID|29726|N|From Little Lu|M|38.2,45.9|; Lair of the Jade Witch
+C Inspect sign |QID|29726||NC|QO|Inspect sign: 1/1|M|38.3,45.5|; Lair of the Jade Witch
+C Inspect Lifelike Jade Statue |QID|29726|NC|QO|Inspect Lifelike Jade Statue: 1/1|M|38.8,45.9|; Lair of the Jade Witch
+C Inspect Suspicious Jade Statue |QID|29726|QO|Inspect Suspicious Jade Statue: 1/1|M|38.9,46.4|; Lair of the Jade Witch
+C Speak with Widow Greenpaw |QID|29726|QO|Speak with Widow Greenpaw: 1/1|M|38.8,46.0|N|Flee Widow Greenpaw as soon as you can.|; Lair of the Jade Witch
+T SI:7 Report: Hostile Natives |QID|29726|N|To Little Lu|M|59.1,81.9|; Pearlfin Village
+A SI:7 Report: Take No Prisoners |QID|29727|N|From Amber Kearnen|M|29.4,54.1|; Pearlfin Village
+C SI:7 Report: Take No Prisoners |QID|29727|N|It's shooting time! Taget the Hozen that have a yellow triangle over their head first.|; Dookin' Grounds
+T SI:7 Report: Take No Prisoners |QID|29727|N|To Amber Kearnen|M|58.9,81.9|; Pearlfin Village
+A A Perfect Match |QID|29903|N|From Admiral Taylor|M|58.9,81.7|; Pearlfin Village
+A Seek Out the Lorewalker |QID|29888|N|From Bold Karasshi|M|59.0,81.7|; Pearlfin Village
+
+C A Perfect Match |QID|29903|S|NC|M|58.7,84.7|N|Speak to the Aqualytes and give them presents. The trick is to look at what weapon they have:\nDaggers: Give a dagger\nSword: Give a shield\nRed Staff: give a healing prayer\nBlue Staff: give a staff|; Pearlfin Village
+
+T Seek Out the Lorewalker |QID|29888|M|53.7,91.5|; Dreamer's Pavilion
+A Borrowed Brew |QID|29889|M|53.7,91.5|; Dreamer's Pavilion
+C Borrowed Brew |QID|29889|NC|M|53.7,91.4|N|Drink the brew and have a booze induced vision|; Dreamer's Pavilion
+T Borrowed Brew |QID|29889|N|To Lorewalker Cho|M|53.7,91.4|; Dreamer's Pavilion
+A A Visit with Lorewalker Cho |QID|31130|N|From Lorewalker Cho|M|53.7,91.4|; Dreamer's Pavilion
+C A Visit with Lorewalker Cho |QID|31130|NC|QO|Trim the Tree: 1/1|M|53.8,91.0|; Dreamer's Pavilion
+C A Visit with Lorewalker Cho |QID|31130|NC|QO|Paint the Wall: 1/1|M|54.0,90.7|N|Choose whatever|; Dreamer's Pavilion
+C A Visit with Lorewalker Cho |QID|31130|NC|QO|Feed the Birds: 1/1|M|54.0,91.3|; Dreamer's Pavilion
+T A Visit with Lorewalker Cho |QID|31130|N|To Lorewalker Cho|M|54.0,91.2|; Dreamer's Pavilion
+
+C A Perfect Match |QID|29903|US|NC|M|58.7,84.7|N|Speak to the Aqualytes and give them presents. The trick is to look at what weapon they have:\nDaggers: Give a dagger\nSword: Give a shield\nRed Staff: give a healing prayer\nBlue Staff: give a staff|; Pearlfin Village
+]]
+end)
