a69bbd63ed651a59cb5a7dd930328c87513586dd
Laotseu <eric.beaudoin@gmail.com>
2012-09-30 19:58:11 -0400

    Valley of the Four Winds and Krasarang Wilds fixes after play testing.

--- WoWPro_Leveling/Alliance/87_88_Rajitazi_KrasarangWilds.lua
+++ WoWPro_Leveling/Alliance/87_88_Rajitazi_KrasarangWilds.lua
@@ -29,12 +29,12 @@ return [[
 N Welcome to Krasarang Wilds|N|This guide assumes you are in Zhu's Watch, (where Dragonwing Dan flew you at the end of Valley of the Four Winds guide).|
 T Ken-Ken|QID|29873|M|76.23,7.13|N|To Ken-Ken. This was accepted from Xiao at the beginning of Valley of the 4 Winds.|
 A What's Eating Zhu's Watch?|QID|30079|M|76.24,7.13|N|From Ken-Ken.|
-C What's Eating Zhu's Watch?|QID|30079|NC|QO|Speak with Mei: 1/1|M|75.86,7.60|N|up the stairs, outside.|
+C What's Eating Zhu's Watch?|QID|30079|NC|QO|Speak with Mei: 1/1|M|75.86,7.60|N|Up the stairs, outside.|
 A Finding Yi-Mo|QID|30080|M|75.86,7.60|N|From Mei Barrelbottom.|
-C What's Eating Zhu's Watch?|QID|30079|NC|QO|Speak with Yun: 1/1|M|75.48,9.26|N|up the stairs, inside.|
+C What's Eating Zhu's Watch?|QID|30079|NC|QO|Speak with Yun: 1/1|M|75.48,9.26|N|Up the stairs, inside.|
 C What's Eating Zhu's Watch?|QID|30079|NC|QO|Speak with Despondent Warden of Zhu: 1/1|M|76.69,8.85|
-C What's Eating Zhu's Watch?|QID|30079|NC|QO|Speak with Sunni|M|76.44,8.27|N|up the stairs, inside.|
-f Zhu's Watch|QID|30078|M|76.7,8.38|N|At Gee Hung.|
+C What's Eating Zhu's Watch?|QID|30079|NC|QO|Speak with Sunni|M|77.5,8.6|N|up the stairs, inside.|
+f Zhu's Watch|QID|99999|M|76.7,8.38|N|At Gee Hung.|
 C Finding Yi-Mo|QID|30080|M|81.25,6.85|NC|
 T Finding Yi-Mo|QID|30080|M|81.31,6.78|N|To Yi-Mo Longbrow.|
 A Cheer Up, Yi-Mo|QID|30082|M|81.30,6.78|N|From Yi-Mo Longbrow.|
@@ -44,19 +44,22 @@ T Cheer Up, Yi-Mo|QID|30082|M|75.90,7.61|N|To Mei Barrelbottom.|
 A Tears of Pandaria|QID|30091|M|75.87,7.63|N|From Mei Barrelbottom.|
 T What's Eating Zhu's Watch?|QID|30079|M|76.24,7.14|N|To Ken-Ken.|
 A Materia Medica|QID|30081|M|76.23,7.12|N|From Ken-Ken.|
+C Tears of Pandaria|QID|30091|S|M|71.99,11.45|
 C Materia Medica|QID|30081|M|72.19,10.68|N|fish are sparklies on the ground where you kill horrors, honeycomb in trees nearby, salty core drop from the horrors.|
-C Tears of Pandaria|QID|30091|M|71.99,11.45|
+C Tears of Pandaria|QID|30091|US|M|71.99,11.45|
 T Tears of Pandaria|QID|30091|M|75.83,7.65|N|To Mei Barrelbottom.|
 A Securing the Province|QID|30083|M|75.83,7.65|N|From Mei Barrelbottom.|
 A Borderlands|QID|30084|M|75.83,7.65|N|From Mei Barrelbottom.|
 T Materia Medica|QID|30081|M|76.24,7.14|N|To Ken-Ken.|
 A Why So Serious?|QID|30088|M|76.24,7.14|N|From Ken-Ken.|
-C Why So Serious?|QID|30088|S|L|78941 18|M|72.00,16.91|
+C Securing the Province|QID|30083|S|M|77.30,58.91|Z|Valley of the Four Winds|
+C Collect Panther Fanks|QID|30088|S|QO|Huge Panther Fang: 18/18|M|72.00,16.91|
 C Borderlands|QID|30084|NC|U|78928|QO|Memorial Flame of Po: 1/1|M|75.20,17.20|
-C Borderlands|QID|30084|NC|U|78928|QO|Memorial Flame of Zhu: 1/1|M|71.21,17.55|
+C Borderlands|QID|30084|NC|U|78928|QO|Memorial Flame of Zhu: 1/1|M|72.7,18.1|
 C Borderlands|QID|30084|NC|U|78928|QO|Memorial Flame of Rin: 1/1|M|71.21,17.55|
-C Why So Serious?|QID|30088|US|M|73.89,56.30|Z|Valley of the Four Winds|
-C Securing the Province|QID|30083|M|77.30,58.91|Z|Valley of the Four Winds|
+C Get the Jar of Pigment|QID|30088|QO|Jar of Pigment: 1/1|M|74.0,56.4|Z|Valley of the Four Winds|
+C Collect Panther Fanks|QID|30088|US|QO|Huge Panther Fang: 18/18|M|72.00,16.91|
+C Securing the Province|QID|30083|US|M|77.30,58.91|Z|Valley of the Four Winds|
 T Securing the Province|QID|30083|M|75.83,7.65|N|To Mei Barrelbottom.|
 T Borderlands|QID|30084|M|75.83,7.65|N|To Mei Barrelbottom.|
 T Why So Serious?|QID|30088|M|76.24,7.14|N|To Ken-Ken.|
@@ -64,7 +67,8 @@ A Apply Directly to the Forehead|QID|30089|M|76.25,7.15|N|From Ken-Ken.|
 C Apply Directly to the Forehead|QID|30089|U|79021|M|76.69,9.11|
 T Apply Directly to the Forehead|QID|30089|M|76.24,7.10|N|To Ken-Ken.|
 A Zhu's Despair|QID|30090|M|76.24,7.10|N|From Ken-Ken.|
-C Zhu's Despair|QID|30090|U|79057|M|76.43,8.76|
+K Kill Essence of Despair|QID|30090|QO|Essence of Despair slain: 8/8|M|76.8,9.6|N||
+C Zhu's Despair |QID|30090|U|79057|QO|Yi-Mo Cured: 1/1|M|76.43,8.76|N|Use Ken-Ken's Mask on Yi-Mo and fight the big Essence.|
 T Zhu's Despair|QID|30090|M|76.23,7.10|N|To Ken-Ken.|
 A Into the Wilds|QID|30178|M|75.81,7.68|N|From Mei Barrelbottom.|
 
@@ -78,11 +82,11 @@ T The Arcanic Oubliette|QID|30274|M|67.34,32.62|N|To Lorekeeper Vaeldrin.|
 A The Lost Dynasty|QID|30344|M|67.34,32.62|N|From Lorekeeper Vaeldrin.|
 A Squirmy Delight|QID|30350|M|67.34,32.67|N|From Kang Bramblestaff.|
 A Blind Them!|QID|30384|M|67.42,32.74|N|From Lyalia.|
-f The Incursion|QID|30384|M|67.78,32.52|N|At Tylen Moonfeather.|
+f The Incursion|QID|99999|M|67.78,32.52|N|At Tylen Moonfeather.|
 C Squirmy Delight|QID|30350|S|M|63.00,27.79|
 C Crane Mastery|QID|30352|S|M|63.94,29.44|
 C Profit Mastery|QID|30353|S|M|71.15,24.65|
-C Blind Them!|QID|30384|M|66.55,25.08|
+K Kill Sunwalker Scout|QID|30384|T|Sunwalker Scout|M|66.55,25.08|
 C Squirmy Delight|QID|30350|US|M|63.00,27.79|
 C Crane Mastery|QID|30352|US|M|63.94,29.44|
 C Profit Mastery|QID|30353|US|M|71.15,24.65|
@@ -92,8 +96,8 @@ A Profit Mastery: Chasheen|QID|31260|M|67.60,25.77|N|From Daggle Bombstrider.|
 T Crane Mastery|QID|30352|M|67.6, 25.77|
 A Crane Mastery: Needlebeak|QID|31262|M|67.61,25.70|N|From Daggle Bombstrider.|
 C The Lost Dynasty|QID|30344|NC|M|68.05,21.47|
-C Crane Mastery: Needlebeak|QID|31262|M|63.19,20.39|
-C Profit Mastery: Chasheen|QID|31260|M|61.66,31.78|
+K Kill Needlebeak|QID|31262|T|Needlebeak|M|63.19,20.39|
+K Kill Chasheen|QID|31260|T|Chasheen|M|61.66,31.78|
 
 T Blind Them!|QID|30384|M|67.33,32.72|N|To Lyalia.|
 T Squirmy Delight|QID|30350|M|67.33,32.72|N|To Kang Bramblestaff.|
@@ -104,18 +108,18 @@ A Lotus Tea|QID|30351|M|67.40,32.68|N|From Kang Bramblestaff.|
 T Crane Mastery: Needlebeak|QID|31262|M|67.61,25.68|N|To Daggle Bombstrider.|
 T Profit Mastery: Chasheen|QID|31260|M|67.61,25.68|N|To Daggle Bombstrider.|
 
-R Ruins of Dojan|QID|31346|M|57.76,36.1|
+R Ruins of Dojan|QID|30346|M|57.76,36.1|
 C Lotus Tea|QID|30351|S|NC|M|55,35|
 C Threat from Dojan|QID|30349|S|M|55,35|
-K Dojani Dominator|QID|30346|M|55.51,35.48|L|78917 1|N|Kill, loot orders.|
+K Dojani Dominator|QID|30346|T|Dojani Dominator|M|55.51,35.48|L|78917 1|N|Kill, loot orders.|
 T Where are the Pools|QID|30346|To UI Alert. {if you miss the alert - it doesn't stay available very long - you can turn in back at the Incursion, but it means an extra run to the ruins and back).|
 A The Pools of Youth|QID|30347|N|from UI Alert.|
-C The Pools of Youth|QID|30347|M|51.63,33.08|N|Chat with Nu Lek, kill the statues that come to life, then click on a water pool to pick it up.|
+C The Pools of Youth|QID|30347|M|54.6,32.9;51.63,33.08|N|Chat with Nu Lek, kill the statues that come to life, then click on a water pool to pick it up.|
 C Threat from Dojan|QID|30349|US|M|55,35|
 C Lotus Tea|QID|30351|US|NC|M|55,35|
-T Lotus Tea|QID|30351|M|To Kang Bramblestaff. (if you dont turn it in right away, you have to wait until you are at sentinal basecamp because Kang goes out of phase, try not to let this happen.)|
+T Lotus Tea|QID|30351|M|66.4,32.5|N|To Kang Bramblestaff. (if you dont turn it in right away, you have to wait until you are at sentinal basecamp because Kang goes out of phase, try not to let this happen.)|
 
-R The Incursion|M|67.4,32|
+R The Incursion|QID|30349|M|67.4,32|
 T Threat from Dojan|QID|30349|M|67.39,32.57|N|To Lyalia.|
 T The Pools of Youth|QID|30347|M|67.39,32.57|N|To Lorekeeper Vaeldrin.|
 A Immortality?|QID|30348|M|67.39,32.57|N|From Lorekeeper Vaeldrin.|
@@ -123,7 +127,7 @@ C Immortality?|QID|30348|M|67.43,32.57|
 T Immortality?|QID|30348|M|67.43,32.57|N|To Lorekeeper Vaeldrin.|
 A Going on the Offensive|QID|30363|M|67.43,32.57|N|From Lyalia.|
 
-R Krasarang River|M|43.83,42.85|
+R Krasarang River|QID|30178|M|43.83,42.85|
 C Going on the Offensive|QID|30363|NC|M|44.01,43.32|
 T Into the Wilds|QID|30178|M|44.24,43.02|N|To Koro Mistwalker.|
 A Unsafe Passage|QID|30269|M|44.16,43.00|N|From Koro Mistwalker.|
@@ -137,19 +141,22 @@ C Tread Lightly|QID|30694|S|U|80828|M|45.77,28.17|
 C Blinding the Riverblades|QID|30270|M|46.14,30.06|
 C Tread Lightly|QID|30694|US|U|80828|M|45.77,28.17|
 C The Murksweats|QID|30268|M|37.33,41.13|
-T The Murksweats|QID|30268|M|40.67,33.88|N|To Anduin Wrynn.|
+T The Murksweats|QID|30268|M|40.5,34.6|N|To Anduin Wrynn.|
 T Tread Lightly|QID|30694|M|40.62,33.69|N|To Koro Mistwalker.|
 T Blinding the Riverblades|QID|30270|M|40.62,33.69|N|To Koro Mistwalker.|
 T The Murksweats|QID|30268|M|40.44,34.58|N|To Anduin Wrynn.|
 A Ahead on the Way|QID|30695|M|40.68,33.68|N|From Koro Mistwalker.|
+A Striking the Rain|QID|30272|M|40.68,33.68|N|From Koro Mistwalker.|
 A Sha Can Awe|QID|30271|M|40.67,33.88|N|From Anduin Wrynn.|
 
-R Temple of the Red Crane|QID|30272|M|40,45|
+R Temple of the Red Crane|QID|30272|M|40.5,45.9|
+C Sha Can Awe|QID|30271|S|M|37.14,49.70|
 C Striking the Rain|QID|30272|S|M|40,45|N|Kill them near the Defenders to get credit for Sha Can Awe also.|
-C Ahead on the Way|QID|30695|QO|West Pagoda Cleansed: 1/1|M|37.82,54.26|
-C Ahead on the Way|QID|30695|M|42.83,54.35|
+C Clean the West Pagoda|QID|30695|QO|West Pagoda Cleansed: 1/1|M|37.82,54.26|
+C Clean the East Pagoda|QID|30695|QO|East Pagoda Cleansed: 1/1|M|42.83,54.35|
 T Ahead on the Way|QID|30695|M|43.00,53.11|N|To Anduin Wrynn.|
-C Sha Can Awe|QID|30271|M|37.14,49.70|
+C Striking the Rain|QID|30272|US|M|40,45|N|Kill them near the Defenders to get credit for Sha Can Awe also.|
+C Sha Can Awe|QID|30271|US|M|37.14,49.70|
 T Ahead on the Way|QID|30695|M|43.00,53.11|N|To Anduin Wrynn.|
 T Striking the Rain|QID|30272|M|38.01,50.89|N|To Anduin Wrynn.|
 T Sha Can Awe|QID|30271|M|38.01,50.89|N|To Anduin Wrynn.|
@@ -160,13 +167,14 @@ T In the House of the Red Crane|QID|30273|M|40.05,48.98|N|To Chi-Ji.|
 R Marista|QID|30667|M|51.7,75.34|
 A Particular Plumage|QID|30667|M|51.70,75.34|N|From Tony Tuna.|
 A Sudden, Unexpected Crocolisk Aggression|QID|30666|M|51.52,76.88|N|From Jay Cloudfall.|
+C Sudden, Unexpected Crocolisk Aggression|QID|30666|S|M|46.34,71.99|
 C Particular Plumage|QID|30667|M|42.57,71.86|
-C Sudden, Unexpected Crocolisk Aggression|QID|30666|M|46.34,71.99|
+C Sudden, Unexpected Crocolisk Aggression|QID|30666|US|M|46.34,71.99|
 T Particular Plumage|QID|30667|M|51.60,75.29|N|To Tony Tuna.|
 T Sudden, Unexpected Crocolisk Aggression|QID|30666|M|51.56,76.87|N|To Jay Cloudfall.|
 A Build Your Own Raft|QID|30668|M|51.56,76.87|N|From Jay Cloudfall.|
 C Build Your Own Raft|QID|30668|NC|QO|Tough Kelp: 1/1|M|52.25,77.78|
-f Marista|QID|30667|M|52.43,76.53|N|At Nan-Po.|
+f Marista|QID|99999|M|52.43,76.53|N|At Nan-Po.|
 C Build Your Own Raft|QID|30668|NC|QO|Spare Plank: 1/1M|52.36,76.21|
 T Build Your Own Raft|QID|30668|M|51.58,76.87|N|To Jay Cloudfall.|
 A The Lorewalker on the Lake|QID|30669|M|51.58,76.87|N|From Jay Cloudfall. Jump to increase your rafts speed.|
@@ -174,32 +182,35 @@ C The Lorewalker on the Lake|QID|30669|M|51.69,76.53|
 T The Lorewalker on the Lake|QID|30669|M|46.02,78.95|N|To Wise Ana Wu.|
 A Wisdom Has A Price|QID|30671|M|46.02,78.95|N|From Wise Ana Wu.|
 A Misery|QID|30691|M|46.02,78.95|N|From Wise Ana Wu.|
-C Misery|QID|30691|M|45.84,79.35;46.27,83.42|CS|
+C Wisdom Has A Price|QID|30671|S|M|46.54,77.47|
+K Kill Nahassa|QID|30691|T|Nahassa|M|45.84,79.35;46.27,83.42|CS|N|Dive, he is in an underwater cave.|
 T Misery|QID|30691|M|46.27,83.42|N|To UI Alert.|
-C Wisdom Has A Price|QID|30671|M|46.54,77.47|
+C Wisdom Has A Price|QID|30671|US|M|46.54,77.47|
 T Wisdom Has A Price|QID|30671|M|46.01,78.96|N|To Wise Ana Wu.|
 A Balance|QID|30672|M|46.01,78.96|N|From Wise Ana Wu. You dont have to stay to watch the dialog.|
 
-R Unga Ingoo|QID|99999|M|45.31,85.17|
-K Unga Fish Getter|T|60299|L|80827|M|45.31,85.17|
-A Balance Without Violence|QID|30674|M|45.31,85.17|N|From UI Alert.|
+;R Unga Ingoo|QID|99999|M|45.31,85.17|
+C Balance|QID|30672|S|M|50.85,84.15|
+K Unga Fish-Getter|QID|30675|T|Unga Fish-Getter|L|80827|M|45.31,85.17|;I don't think this step is really needed.
+A Balance Without Violence|QID|30674|M|45.3,85.6|N|From UI Alert.|
 A Buried Hozen Treasure|QID|30675|U|80827|M|43.81,85.41|N|From Confusing Treasure Map - looted off of an Unga.|
 C Balance Without Violence|QID|30674|NC|S|M|42.68,82.74|
-C Buried Hozen Treasure|QID|30675|NC|S|M|51.10,85.27|
-C Balance|QID|30672|M|50.85,84.15|
-T Balance Without Violence|QID|30674|M|42.22,82.87|N|To UI Alert.|
+C Buried Hozen Treasure|QID|30675|NC|M|51.10,85.27|
 T Buried Hozen Treasure|QID|30675|M|51.10,85.27|N|To UI Alert.|
-R Marista|QID|30672|M|51.55,76.83|Don't forget to jump while on the raft.|
+C Balance Without Violence|QID|30674|NC|US|M|42.68,82.74|
+T Balance Without Violence|QID|30674|M|42.22,82.87|N|To UI Alert.|
+C Balance|QID|30672|US|M|50.85,84.15|
+;R Marista|QID|30672|M|51.55,76.83|Don't forget to jump while on the raft.|
 T Balance|QID|30672|M|51.55,76.83|N|To Jay Cloudfall.|
 
-R Explore|QID|99999|N|If you want, explore the islands while you are out here and have the raft(buff duration 1 hour). You need to check this step off manually.|
-f Cradle of Chi-Ji|QID|30675|M|31.19,63.25|N|At Feather Keeper Li.|
-R Fallsong Village|M|29.66,39.0|N|You can take your raft up the river (portage around the waterfall).|
+R Explore|QID|30168|N|If you want, explore the islands while you are out here and have the raft(buff duration 1 hour). You need to check this step off manually.|
+f Cradle of Chi-Ji|QID|99999|M|31.19,63.25|N|At Feather Keeper Li.|
+R Fallsong Village|QID|30168|M|29.66,39.0|N|You can take your raft up the river (portage around the waterfall).|
 A Thieving Raiders|QID|30168|M|29.66,39.00|N|From Tired Shushen.|
 A Raid Leader Slovan|QID|30169|M|29.66,39.00|N|From Tired Shushen.|
-K Riverblade Flesh-Hunter|QID|30168|L|78958 5|S|M|29.55,45.17|N|If you still have the raft buff, you can go across the water.)
-K Slovan|QID|30169|T|58285|M|31.87,29.01|QO|Slovan slain: 1/1|
-K Riverblade Flesh-Hunter|QID|30168|L|78958 5|S|M|29.55,45.17|
+C Riverblade Flesh-Hunter|QID|30168|L|78958 5|S|M|29.55,45.17|N|If you still have the raft buff, you can go across the water.)
+K Kill Slovan|QID|30169|T|Slovan|M|31.87,29.01|QO|Slovan slain: 1/1|
+C Riverblade Flesh-Hunter|QID|30168|L|78958 5|US|M|29.55,45.17|
 T Thieving Raiders|QID|30168|M|29.65,38.99|N|To Tired Shushen.|
 T Raid Leader Slovan|QID|30169|M|29.65,38.99|N|To Tired Shushen.|
 
@@ -207,7 +218,7 @@ R Sentinel Basecamp|QID|30363|M|24.76,34.00|
 T Going on the Offensive|QID|30363|M|24.76,34.00|N|To Lyalia.|
 A No Sister Left Behind|QID|30354|M|24.76,34.00|N|From Lyalia.|
 A Sever Their Supply Line|QID|30356|M|24.76,34.00|N|From Lyalia.|
-f Sentinel Basecamp|QID|30356|M|25.13,33.53|N|At Maylen Moonfeather.|
+f Sentinel Basecamp|QID|99999|M|25.13,33.53|N|At Maylen Moonfeather.|
 A Re-Reclaim|QID|30355|M|25.22,34.52|N|From Kang Bramblestaff.|
 C Re-Reclaim|QID|30355|NC|S|M|23.43,45.24|
 C No Sister Left Behind|QID|30354|NC|S|M|23.49,45.07|
@@ -221,14 +232,14 @@ T Sever Their Supply Line|QID|30356|M|24.74,34.08|N|To Lyalia.|
 A The Mantid|QID|30361|M|24.88,34.29|N|From Ambassador Han.|
 
 C The Mantid|QID|30361|S|M|13.58,36.40|
-R Shattered Convoy|M|15.94,39.83|
+R Shattered Convoy|QID|30357|M|15.94,39.83|
 A The Stoneplow Convoy|QID|30357|M|15.08,39.32|N|From Lorekeeper Vaeldrin.|
 C The Stoneplow Convoy|QID|30357|U|79163|M|15.62,34.96|
-C The Mantid|QID|30361|M|14.44,35.46|
+C The Mantid|QID|30361|US|M|14.44,35.46|
 T The Mantid|QID|30361|M|15.67,39.77|N|To Ambassador Len.|
 T The Stoneplow Convoy|QID|30357|M|15.14,39.38|N|To Lorekeeper Vaeldrin.|
 A The Lord Reclaimer|QID|30359|M|15.12,39.32|N|From Lorekeeper Vaeldrin.|
-C The Lord Reclaimer|QID|30359|M|23.35,48.80|
+K Kill Groundbreaker Brojai|QID|30359|M|23.35,48.80|N|Talk to Lyalia to summon Groundbreaker Brojai.|
 T The Lord Reclaimer|QID|30359|M|24.74,33.96|N|To Lorekeeper Vaeldrin.|
 A The Waters of Youth|QID|30445|M|24.74,33.96|N|From Lorekeeper Vaeldrin.|
 C The Waters of Youth|QID|30445|M|24.74,33.96|
@@ -240,18 +251,19 @@ T Warn Stoneplow|QID|30360|M|19.54,56.92|Z|Valley of the Four Winds|N|To Loon Ma
 A The Mantidote|QID|30623|M|17.97,56.34|Z|Valley of the Four Winds|N|From Mei Barrelbottom.|
 A The Swarm Begins|QID|30622|M|17.90,56.21|Z|Valley of the Four Winds|N|From Loon Mai.|
 A It Does You No Good In The Keg|QID|30624|M|18.05,55.69|Z|Valley of the Four Winds|N|From Mudmug.|
+C The Swarm Begins|QID|30622|S|M|13.3,54.1|Z|Valley of the Four WInds|
 C The Mantidote|QID|30623|S|U|80337|M|10.72,51.9|Z|Valley of the Four Winds|N|Use the mask on the elites, kill the sha that appears.|
-C It Does You No Good In The Keg|QID|30624|NC|QO|Deliver beer to Crane Wing Priests: 1/1|M|15.09,55.41|Z|Valley of the Four Winds|
-C It Does You No Good In The Keg|QID|30624|NC|QO|Deliver beer to night elf settlers: 1/1|M|15.09,50.1|Z|Valley of the Four Winds|
-C It Does You No Good In The Keg|QID|30624|NC|QO|Deliver beer to the Shado-Pan: 1/1|M|10.95,55.9|Z|Valley of the Four Winds|
-C It Does You No Good In The Keg|QID|30624|NC|QO|Deliver beer to Zhu's Wardens: 1/1|M|17.01,50.46|Z|Valley of the Four Winds|
+C Deliver beer to Zhu's Wardens|QID|30624|NC|QO|Deliver beer to Zhu's Wardens: 1/1|M|17.01,50.46|Z|Valley of the Four Winds|
+C Deliver beer to night elf settlers|QID|30624|NC|QO|Deliver beer to night elf settlers: 1/1|M|15.09,50.1|Z|Valley of the Four Winds|
+C Deliver beer to Crane Wing Priests|QID|30624|NC|QO|Deliver beer to Crane Wing Priests: 1/1|M|15.09,55.41|Z|Valley of the Four Winds|
+C Deliver beer to the Shado-Pan|QID|30624|NC|QO|Deliver beer to Shado-Pan: 1/1|M|10.95,55.9|Z|Valley of the Four Winds|
 T It Does You No Good In The Keg|QID|30624|N|To UI Alert.|
-C The Swarm Begins|QID|30622|M|12.27,49.65|Z|Valley of the Four WInds|
 C The Mantidote|QID|30623|US|U|80337|M|10.72,51.92|Z|Valley of the Four Winds|
 T The Mantidote|QID|30623|N|UI Alert|
+C The Swarm Begins|QID|30622|US|M|13.3,54.1|Z|Valley of the Four WInds|
 T The Swarm Begins|QID|30622|M|11.53,49.57|Z|Valley of the Four Winds|N|To Guard Captain Oakenshield.|
 A Students No More|QID|30625|M|11.53,49.57|Z|Valley of the Four Winds|N|From Guard Captain Oakenshield.|
-C Students No More|QID|30625|M|8.97,51.01|Z|Valley of the Four Winds|N|The NPC's will call you over to join them as they roam around this area killing these 4 elites.|
+C Students No More|QID|30625|T|Xiao|M|8.9,51.7;9.7,58.3;9.4,54.2;9.6,54.0|CC|Z|Valley of the Four Winds|N|The NPC's will call you over to join them as they roam around this area killing these 4 elites.|
 T Students No More|QID|30625|M|11.55,49.44|Z|Valley of the Four Winds|N|To Guard Captain Oakenshield.|
 A Retreat!|QID|30626|M|11.55,49.44|Z|Valley of the Four Winds|N|From Guard Captain Oakenshield.|
 T Retreat!|QID|30626|M|17.91,56.22|Z|Valley of the Four Winds|N|To Loon Mai.|
