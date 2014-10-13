5b28503d77482a7ec3888afc7e84137b06494767
Laotseu <eric.beaudoin@gmail.com>
2014-10-13 06:53:32 -0400

    I missed a fix to GetQuestLogTitle().

--- WoWPro/WoWPro_Events.lua
+++ WoWPro/WoWPro_Events.lua
@@ -237,7 +237,7 @@ function WoWPro:AutoCompleteQuestUpdate(questComplete)
 				        -- end
 				
 						-- Quest Completion: Any C, K or l step is considered completed if the quest is completed
-						elseif (action == "C" or action == "K" or action == "l") and not completion and quest_log_index and select(7,GetQuestLogTitle(quest_log_index)) == 1 then
+						elseif (action == "C" or action == "K" or action == "l") and not completion and quest_log_index and select(6,GetQuestLogTitle(quest_log_index)) == 1 then
 							WoWPro.CompleteStep(i, true)
 
 						-- If the flighpoint is already known, mark it. Not really related to the quest log but only
